be268a31c8fcf5c7e6ea4e272490e2bd565efc50
fgd-haha <fgd.freedom@gmail.com>
2022-03-10 23:17:56 +0800

    feat: 绘制导览界面

--- app/src/main/java/fgd/showroom/ui/guide/GuideFragment.kt
+++ app/src/main/java/fgd/showroom/ui/guide/GuideFragment.kt
@@ -1,48 +1,89 @@
 package fgd.showroom.ui.guide
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import fgd.showroom.MainActivity
+import fgd.showroom.R
 import fgd.showroom.databinding.FragmentGuideBinding
+import fgd.showroom.logic.model.Step
 
 class GuideFragment : Fragment() {
-
-    private lateinit var guideViewModel: GuideViewModel
+    val viewModel by lazy { ViewModelProvider(this)[GuideViewModel::class.java] }
     private var _binding: FragmentGuideBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private var stepList = mutableListOf<Step>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        guideViewModel =
-            ViewModelProvider(this).get(GuideViewModel::class.java)
-
         _binding = FragmentGuideBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        guideViewModel.text.observe(viewLifecycleOwner, {
-            textView.text = it
-        })
+        viewModel.stepList.observe(viewLifecycleOwner) { items ->
+            Log.d("SettingFragment", items.toString())
+            stepList = items
+            dispStepList()
+        }
+
         return root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        activity?.title = "游览模式"
+    override fun onResume() {
+        super.onResume()
+        if (activity is MainActivity) {
+            activity?.title = "游览模式"
+        } else activity?.title = "设置步骤位置"
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+
+    @SuppressLint("SetTextI18n")
+    private fun dispStepList() {
+        if (stepList.size <= 0) return
+
+        val dm = DisplayMetrics()
+        this.requireActivity().windowManager.defaultDisplay.getMetrics(dm)
+        val width = dm.widthPixels
+        val height = dm.heightPixels
+
+        for (i in stepList.indices) {
+            val step = stepList[i]
+            val textView = TextView(this.context)
+
+            textView.id = step.stepno
+            textView.text = "${step.stepno} ${step.stepname}"
+
+            textView.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.icon_red_72_45, 0, 0);
+
+            val tvParams = RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+            )
+            if (step.posx > 0 && step.posy > 0) {
+                if (step.posx > width - 150) tvParams.leftMargin = width - 150 else tvParams.leftMargin = step.posx
+                if (step.posy > height - 250) tvParams.topMargin = height - 250 else tvParams.topMargin = step.posy
+            } else {
+                tvParams.leftMargin = 100
+                tvParams.topMargin = 100
+            }
+
+            binding.root.addView(textView, tvParams)
+//            btn.setOnTouchListener(mBtnListener)
+        }
     }
 }
--- app/src/main/java/fgd/showroom/ui/guide/GuideViewModel.kt
+++ app/src/main/java/fgd/showroom/ui/guide/GuideViewModel.kt
@@ -1,13 +1,15 @@
 package fgd.showroom.ui.guide
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import fgd.showroom.logic.Repository
+import fgd.showroom.logic.model.Step
+import kotlinx.coroutines.launch
 
 class GuideViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is guide Fragment"
-    }
-    val text: LiveData<String> = _text
+    private val _stepList = MutableLiveData<MutableList<Step>>().apply { viewModelScope.launch { value = Repository.getStepList() } }
+
+    val stepList = _stepList
 }
